57c289a7b7057a58d99fa946c8f4a5beec04b0b1
Vladimir Grygoriev <vgrygoriev@qualium-systems.com>
2024-08-08 17:55:35 +0300

    Add project with Crime Fragment and Host Activity

---
+++ app/src/main/java/com/vg/criminal_intent/Crime.kt
@@ -0,0 +1,11 @@
+package com.vg.criminal_intent
+
+import java.util.Date
+import java.util.UUID
+
+data class Crime(val id : UUID = UUID.randomUUID(),
+                 var title : String = "",
+                 var date : Date = Date(),
+                 var isSolved : Boolean = false
+                ) {
+}
